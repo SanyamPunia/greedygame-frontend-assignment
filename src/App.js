f847f2be5f498b01819c8e068ee1d3ea6c9d73bc
SanyamPunia <lewarends@gmail.com>
2022-09-09 13:16:54 +0530

    remove selected state

--- src/App.js
+++ src/App.js
@@ -6,61 +6,51 @@ const tags = [
     {
         id: 1,
         tagName: 'Name',
-        selected: true,
         visibleState: false,
     },
     {
         id: 2,
         tagName: 'Date',
-        selected: true,
         visibleState: false,
     },
     {
         id: 3,
         tagName: 'App',
-        selected: true,
         visibleState: false,
     },
     {
         id: 4,
         tagName: 'Clicks',
-        selected: true,
         visibleState: false,
     },
     {
         id: 5,
         tagName: 'Ad Requests',
-        selected: true,
         visibleState: false,
     },
     {
         id: 6,
         tagName: 'Ad Response',
-        selected: true,
         visibleState: false,
     },
     {
         id: 7,
         tagName: 'Impression',
-        selected: true,
         visibleState: false,
     },
     {
         id: 8,
         tagName: 'Revenue',
-        selected: true,
         visibleState: false,
     },
     {
         id: 9,
         tagName: 'Fill Rate',
-        selected: true,
         visibleState: false,
     },
     {
         id: 10,
         tagName: 'CTR',
-        selected: true,
         visibleState: false,
     },
 ]
